ae7cefb834a45bc8a21f9d82fa4b0f3524ea90a2
Info Zetra <info@zetra.io>
2023-07-17 20:58:44 +0200

    Added fabfile

---
+++ fabfile.py
@@ -0,0 +1,162 @@
+"""
+Automate deployments, rollbacks and updates 
+
+Author: Zetra
+Date  : 2023-07-17
+"""
+
+from dotenv import load_dotenv
+from fabric import Connection, task
+from invoke import Responder
+from os import environ, path, getcwd
+
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
+
+# Get the environment variables
+PEM_KEY_DIR      = environ.get("PEM_KEY_DIR")
+PEM_KEY          = environ.get("PEM_KEY")
+HOST_NAME        = environ.get("HOST_NAME")
+ROOT_USERNAME    = environ.get("HOST_USERNAME")
+USER_NAME        = environ.get("USERNAME")
+GIT_ZETRALERT_URL = environ.get("GIT_ZETRALERT_URL")
+
+print(path.join(PEM_KEY_DIR,PEM_KEY))
+
+
+def _create_zetralert_user():
+    """
+    Create a user called zetralert. This user will be used to SSH as opposed to Admin
+    """
+    with Connection(
+        HOST_NAME,
+        user=ROOT_USERNAME,
+        connect_kwargs={"key_filename": path.join(PEM_KEY_DIR, PEM_KEY)}
+    ) as c:
+
+        """
+        Create the zetralert user
+        """
+        #c.run("sudo adduser zetralert --disabled-password --home /var/zetralert --gecos ''")       
+
+        c.sudo("mkdir -p /var/zetralert/.ssh", user='zetralert')
+
+        c.sudo("chmod 700 /var/zetralert/.ssh",user='zetralert')
+
+        c.sudo("touch /var/zetralert/.ssh/authorized_keys", user='zetralert')
+
+        c.sudo("chmod 600 /var/zetralert/.ssh/authorized_keys", user="zetralert")
+
+        print("Copying the keys")
+        c.run("sudo cp .ssh/authorized_keys /var/zetralert/.ssh/authorized_keys")
+
+        print("==== zetralert User has been setup =====")   
+        
+        
+def _setup_new_instance():
+    
+    """ 
+    Setup the server for the application  
+    
+    Run updates
+    Install Python
+    Create the new user
+    
+    """
+    with Connection(
+        HOST_NAME,
+        user=ROOT_USERNAME,
+        connect_kwargs={"key_filename": path.join(PEM_KEY_DIR, PEM_KEY)}
+    ) as c:
+        
+        print("===== STARTING SETUP ========")
+        # Run updates
+        c.run("sudo apt update -y")
+        
+        """
+        Create the zetralert user
+        """
+        c.run("sudo adduser zetralert --disabled-password --home /var/zetralert --gecos ''")
+        _create_zetralert_user()
+        
+        
+        # Install packages
+        c.run("sudo apt-get install git build-essential python3-dev python3-venv python3 python3-pip python3.7-venv python3.7-dev libxml2-dev libxslt1-dev libffi-dev zlib1g-dev libssl-dev gettext libpq-dev libmariadb-dev libjpeg-dev libopenjp2-7-dev -y")
+
+        """
+        Install Python3.7
+        """
+        c.run("sudo apt install software-properties-common -y")
+        c.run("sudo add-apt-repository ppa:deadsnakes/ppa -y")
+
+        c.run("sudo apt install python3.7 -y")
+        
+        print("==== SETUP COMPLETE =====")  
+        
+        
+def _deploy_new_zetralert_instance():
+    """ Deploy new instance from Git """
+    
+    with Connection(
+        HOST_NAME,
+        user=USER_NAME,
+        connect_kwargs={"key_filename": path.join(PEM_KEY_DIR, PEM_KEY)}
+    ) as c:
+        
+        # Make the virtual env directory
+        c.run("mkdir -p '/var/zetralert/venvs/' && exit")
+        
+        
+        # Get the latest stable version of Vibin-Pretix
+        print("Clone the Zetralert repo")
+        c.run("git clone "+ GIT_ZETRALERT_URL + " && exit")
+        print("Repo successfully cloned")
+        
+        # Create the virtual environment
+        print("Create the virtual environment")
+        c.run("python3.7 -m venv /var/zetralert/venvs/venv && exit")
+        print("Virtual environment created")
+        
+        # Install requirments
+        print("Install the requirements")
+        c.run("source venvs/venv/bin/activate && cd zetralerts && pip3 install -r requirements.txt && exit") 
+        
+        print("==== DEPLOY COMPLETE =====")  
+        
+        
+def _start_zetralert():
+    """ Start Zetralert """
+    
+    with Connection(
+        HOST_NAME,
+        user=USER_NAME,
+        connect_kwargs={"key_filename": path.join(PEM_KEY_DIR, PEM_KEY)}
+    ) as c:
+        
+         c.run("source /var/zetralert/venvs/venv/bin/activate && cd /var/zetralert/zetralerts && " + "nohup python3.7 main.py & " + " && exit")
+         print("Zetralerts has been started...")
+    
+def _stop_zetralert():
+    """ Stop Zetralert """
+    
+    with Connection(
+        HOST_NAME,
+        user=USER_NAME,
+        connect_kwargs={"key_filename": path.join(PEM_KEY_DIR, PEM_KEY)}
+    ) as c:
+        pass 
+    
+    
+@task
+def deploy_start_zetralerts():
+    """
+    Deploy and start a new instanc of Zetralerts
+    """
+    
+    _setup_new_instance()
+    
+    _deploy_new_zetralert_instance()
+    
+    _start_zetralert()
